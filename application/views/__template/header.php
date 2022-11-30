e6cff0efeed6369f8017de8b950b4bb12ffba5dd
IST-PC-25 <IST-PC-25@ad.hsinni.com>
2022-11-30 14:52:17 +0700

    fixed-all

--- application/views/__template/header.php
+++ application/views/__template/header.php
@@ -30,9 +30,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?php echo base_url() ?>assets/adminlte/index3.html" class="brand-link">
+    <a href="<?php echo base_url() ?>index.php/barang" class="brand-link">
       <img src="<?php echo base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Hmmm</span>
     </a>
 
     <!-- Sidebar -->
